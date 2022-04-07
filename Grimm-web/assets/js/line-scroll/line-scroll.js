4847452f0d8c54fbd9711694d435ce58725ad6c1
franzbruno95 <59353843+franzbruno95@users.noreply.github.com>
2022-04-07 15:13:29 +0200

    update

---
+++ Grimm-web/assets/js/line-scroll/line-scroll.js
@@ -0,0 +1,24 @@
+// DEFINITION UNSERER ELEMENTE
+const SVGLOGO = document.getElementById('logo');
+
+// DEFINITION VON VARIABLEN
+const LOGOLENGTH = SVGLOGO.getTotalLength();
+
+// GRUNDLEGENDES SETTING
+SVGLOGO.style.strokeDasharray = LOGOLENGTH;
+SVGLOGO.style.strokeDashoffset = LOGOLENGTH;
+
+// ZEICHNEN UNSERES SVG GRAFEN
+const drawWhenScroll = () => {
+    const DRAWLOGO = LOGOLENGTH * calcScrollPercent();
+    SVGLOGO.style.strokeDashoffset = LOGOLENGTH - DRAWLOGO;
+}
+
+// FUNKTION UM EINEN % WERT DES SCROLLS ZU BERECHNEN
+const calcScrollPercent = () => {
+    let height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    return document.documentElement.scrollTop / height;
+}
+
+// FUNKTION FÜR DAS TRIGGERS DES SCROLL EVENTS
+window.addEventListener('scroll', drawWhenScroll);
